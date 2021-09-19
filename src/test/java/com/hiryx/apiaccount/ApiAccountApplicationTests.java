dc5f588930de59076f07e510f2a9f7f4c5b5c2b7
Alexis TADIFO <alexis.tadifo@gmail.com>
2021-09-19 01:58:54 -0400

    Init of the API with Spring Intializr

---
+++ src/test/java/com/hiryx/apiaccount/ApiAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiryx.apiaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
